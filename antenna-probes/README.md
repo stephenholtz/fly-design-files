a5edab1ef1b1dfc4761e473f088e16b82077a26c
Stephen Holtz <git@holtz.email>
2016-06-28 17:50:49 -0400

    update resonance calc and docs

--- antenna-probes/README.md
+++ antenna-probes/README.md
@@ -3,7 +3,9 @@ antenna-probes
 - designed to screw directly into the M3 threads on PI piezos
 - grounding issues may require intermediate adapter
 - manufactured custom from Carbide Probes Inc, they're great
-- *.m file has estimate of radial (cantilever) 1st resonant mode ~1.5-2kHz
- 
+
+####Cantilevered rod resonance
+- *.m file has estimate of radial (cantilevered) 1st resonant mode
+- conservatively estimated around 2.3kHz
 
 ![Inventor Preview](./r01/carbide_probe_30deg_tip_r01.png?raw=true "Inventor Preview")
--- antenna-probes/carbide_probe_res_calc.m
+++ antenna-probes/carbide_probe_res_calc.m
@@ -15,25 +15,27 @@
 % Poisson's Ratio 20° C         ?                ASTM-C848       0.22
 % Thermal Conductivity 20 °C    W/m °K           ASTM-C408       85
 
+% http://www.roymech.co.uk/Useful_Tables/Vibrations/Natural_Vibrations.html
 p = 14.5*1000; % gm/cm^3 -> kg/m^3
 
 % Pa = kg*m^-1*s^-2
 E = 593*1E9; % Gpa -> pa
 
 % Len inches... b/n .65 and 1.03
-Lin = .9; 
-L = Lin*2.54/100; % in->m
+Lin = [1.03 .74];
+L = Lin.*2.54/100; % in->m
 
 % Diameter inches;
-Din = 0.07; 
-A = (Din*2.54/100/2)^2 * pi; % in->m^2
+Din = 0.07/1;
+D = Din*2.54/100;
 
-% Rod moment of inertia (at end)... with negligible mass, is that OK?
-M = A*L*p; % mass in kg
-I = (1/3)*M*L^2; % moment in kg*m^2
+M = A*L*p./L; % mass kg/m
 
-% Frequency in Hertz 
-f = 3.52/(2*pi*L^2) * sqrt((E*I)/(p*A));
-disp(f)
+% Mass moment of inertia of a rod
+I = (pi/64)*D^4;
+
+% Frequency in Hertz
+f = 3.52./(2*pi*L.^2) .* sqrt(E*I/(p*A));
+fprintf('%fkHz\n',f/1000)
 
 % likely somewhere between 1.5kHz and 2kHz
